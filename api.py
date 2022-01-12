4c8517cd07e9448c0784c502b8886066eab10c01
Ldeng928 <lideng928@gmail.com>
2022-01-12 11:58:13 -0500

    Added edit funtionality on recipe

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request, abort, redirect, url_for, flash
 import sqlite3
 from sqlite3 import Error
-from wtforms import Form, StringField, validators
+from wtforms import Form, StringField, form, validators
 
 # Connect to database
 
@@ -104,6 +104,49 @@ def create_recipe():
     elif request.method == 'GET':
         # return the form to the template
         return render_template('create-recipe.html', form=form)
+
+# Edit recipe
+
+
+@app.route('/recipe/edit/<id>/', methods=['POST', 'GET'])
+def edit_recipe(id):
+    # Fetch data into the form
+    connection = create_connection("recipes.db")
+    cursor = connection.cursor()
+    # Get recipe by id
+    select_one_recipe = "SELECT * FROM recipes WHERE id=?"
+    result = cursor.execute(select_one_recipe, (id,))
+    recipe = result.fetchone()
+    print(recipe)
+
+    # Get form
+    form = CreateRecipeForm(request.form)
+    form.title.data = recipe[1]
+    form.image.data = recipe[2]
+    form.link.data = recipe[3]
+
+    if request.method == 'POST' and form.validate():
+        title = request.form['title']
+        image = request.form['image']
+        link = request.form['link']
+
+        connection = create_connection("recipes.db")
+        cursor = connection.cursor()
+
+        update_a_recipe = "UPDATE recipes SET title=?, image=?, link=? WHERE id=?"
+        cursor.execute(update_a_recipe, (title, image, link, id))
+
+        # Commit to db
+        connection.commit()
+
+        # Flash message
+        flash('Recipe Updated', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('edit-recipe.html', form=form)
+
+
+# Delete recipe
 
 
 @app.route('/recipe/delete/<id>/', methods=['POST'])
